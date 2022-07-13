c0401857db6cbd508c54c91acef648e1702e1bdc
Vladimir-Sluchko <vovan1991.s@gmail.com>
2022-07-13 21:26:01 +0300

    edit by Dao Events

--- afisha/src/main/java/by/itacademy/afisha/dao/api/IConcertDao.java
+++ afisha/src/main/java/by/itacademy/afisha/dao/api/IConcertDao.java
@@ -1,16 +1,17 @@
 package by.itacademy.afisha.dao.api;
 
-import by.itacademy.afisha.dao.apiEvent.IEventDao;
 import by.itacademy.afisha.dao.entity.Concert;
 import by.itacademy.afisha.dao.entity.enums.Type;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.UUID;
 
 @Repository
-public interface IConcertDao extends IEventDao<Concert> {
+public interface IConcertDao extends JpaRepository<Concert, UUID> {
     List<Concert> findByType(Type type);
     Page<Concert> findByType(Type type, Pageable pageable);
 }
--- afisha/src/main/java/by/itacademy/afisha/dao/api/IFilmDao.java
+++ afisha/src/main/java/by/itacademy/afisha/dao/api/IFilmDao.java
@@ -1,16 +1,17 @@
 package by.itacademy.afisha.dao.api;
 
-import by.itacademy.afisha.dao.apiEvent.IEventDao;
 import by.itacademy.afisha.dao.entity.Film;
 import by.itacademy.afisha.dao.entity.enums.Type;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.UUID;
 
 @Repository
-public interface IFilmDao extends IEventDao<Film> {
+public interface IFilmDao extends JpaRepository<Film, UUID> {
     List<Film> findByType(Type type);
     Page<Film> findByType(Type type, Pageable pageable);
 
